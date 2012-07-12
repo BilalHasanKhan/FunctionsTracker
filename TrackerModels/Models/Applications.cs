4a992d8751f90ecceef8dbe5ec14c092e485d977
SadafAnwer <sadafanwer12@gmail.com>
2012-07-12 15:32:13 +0530

    Changes done by Sadaf

--- TrackerModels/Models/Applications.cs
+++ TrackerModels/Models/Applications.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,16 +14,37 @@ namespace TrackerModels.Models
             
             [MaxLength(30)]
             public string AppName { get; set; }
-            public int SLALevel { get; set; }
-            public string CMDBRefNum { get; set; }           
+
+            [MaxLength(20)]
+            public string AppInstance { get; set; }
+
+
+            [MaxLength(10)]
+            public string CMDBRefNum { get; set; }
+
+           
+            public int SLALevel { get; set; }    
             public int ServerId { get; set; }
             public int URLId { get; set; }
             public int StakeHolderMappingId { get; set; }          
             public int OrgId { get; set; }
+            public int UserId { get; set; }
+           
+            
+            public virtual ICollection<StakeHoldersDetails> _StakeHoldersDetails { get; set; }
+            public virtual ICollection<Users> _Users { get; set; }
+
+
+            public Applications()
+            {
+                
+                _StakeHoldersDetails = new HashSet<StakeHoldersDetails>();
+                _Users = new HashSet<Users>();
+            }
+
 
             [ForeignKey("ServerId")]
             public virtual ServerDetails serverDetails { get; set; }
-
 
             [ForeignKey("URLId")]
             public virtual URLDetails urlDetails { get; set; }
@@ -33,6 +54,10 @@ namespace TrackerModels.Models
 
             [ForeignKey("OrgId")]
             public virtual OrgDetails orgDetails { get; set; }
+
+            [ForeignKey("UserId")]
+            public virtual Users users { get; set; }
        
     }
 }
+
